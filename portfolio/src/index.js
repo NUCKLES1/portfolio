a88d495469b86a304bf9bc219f3dddea0a1e2449
NUCKLES1 <danyxcharge@gmail.com>
2023-05-14 15:08:39 -0700

    deploy

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Routing from './Routing';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <BrowserRouter>
-    <Routing  />
+    <App  />
   </BrowserRouter>
   </React.StrictMode>
 );
